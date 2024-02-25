f20465d152c113a7aaa1c541e206499116d78639
CyberFather <maks12ate@gmail.com>
2024-02-25 19:02:05 +0300

    Download rfc, store in fs, and show to user

--- lua/rfc/init.lua
+++ lua/rfc/init.lua
@@ -3,15 +3,21 @@ local M = {}
 local has_plenary, plenary = pcall(require, "plenary")
 
 if not has_plenary then
-    error("This plugin requires nvim-lua/plenary.nvim")
+    error("This plugin requires nvim-lua/plenary.nvim to work.")
 end
 
-local file_exists = function(name)
-   local f=io.open(name,"r")
-   if f~=nil then io.close(f) return true else return false end
+local has_telescope, telescope = pcall(require, "telescope")
+
+if not has_telescope then
+    error("This plugins requires nvim-telescope/telescope.nvim to work.")
 end
 
 local config = {}
+
+local file_exists = function(name)
+   local f = io.open(name,"r")
+   if f ~= nil then io.close(f) return true else return false end
+end
 
 local function slice(tbl, first, last, step)
     local sliced = {}
@@ -24,7 +30,7 @@ end
 local function sed(writer)
     plenary.job:new({
         command = "sed",
-        args = { "-sr", [[s/RFC([0-9]+) \|.*\|.*, "(.*)".* DOI 10\.17487\/RFC[0-9]+\, (.*), .*/RFC_\1 \"\2\" \3/]] },
+        args = { "-sr", [[s/RFC([0-9]+) \|.*\|.*, "(.*)".*/RFC\1:\2/]] },
         writer = writer:result(),
         on_exit = function(j, _)
             local file = io.open(config.rfc_dir .. "/rfc-ref.txt", "w")
@@ -37,10 +43,9 @@ local function sed(writer)
 end
 
 M.download_index = function()
-    local x = config.exe_for_download
     plenary.job:new({
-        command = x[1],
-        args = { x[2], "https://www.ietf.org/rfc/rfc-ref.txt" },
+        command = "curl",
+        args = { "https://www.ietf.org/rfc/rfc-ref.txt" },
         on_exit = function(j, return_val)
             if return_val == 0 then
                 sed(j)
@@ -51,6 +56,18 @@ M.download_index = function()
     }):start()
 end
 
+M.download_rfc = function(rfc)
+    plenary.job:new({
+        command = "wget",
+        args = { "-O", config.rfc_dir .. "/rfc" .. rfc .. ".txt", "https://www.ietf.org/rfc/rfc" .. rfc .. ".txt" },
+        on_exit = function(j, return_val)
+            if return_val ~= 0 then
+                error("Failed to download RFC")
+            end
+        end,
+    }):sync()
+end
+
 M.setup = function(opts)
     config.rfc_dir = opts.rfc_dir or vim.fn.stdpath("data") .. "/rfc.nvim"
 
@@ -58,22 +75,87 @@ M.setup = function(opts)
         vim.fn.mkdir(config.rfc_dir, "p")
     end
 
-    if vim.fn.executable("curl") == 1 then
-        config.exe_for_download = { "curl", "-s", "-o" }
-    elseif vim.fn.executable("wget") == 1 then
-        config.exe_for_download = { "wget", "-q", "-O" }
-    else
-        error("This plugin requires curl or wget")
+    if vim.fn.executable("curl") ~= 1 then
+        error("This plugin requires curl")
     end
 
     if vim.fn.executable("sed") == 0 then
         error("This plugin requires sed")
     end
 
-
     if not file_exists(config.rfc_dir .. "/rfc-ref.txt") then
         M.download_index()
     end
+
+end
+
+local pickers = require("telescope.pickers")
+local finders = require("telescope.finders")
+local action_state = require("telescope.actions.state")
+local sorters = require('telescope.sorters')
+
+local open_rfc_buf = function(file_path, rfc)
+    local file = io.open(file_path, "r")
+    local file_content = file:read("a")
+    local bufnr = vim.api.nvim_create_buf(true, true)
+
+    vim.api.nvim_buf_set_name(bufnr, "rfc" .. rfc .. ".txt")
+    vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, vim.split(file_content, "\n"))
+
+    vim.api.nvim_buf_attach(bufnr, false, {
+        on_detach = function(_, _)
+            print("Detached", bufnr)
+            vim.api.nvim_buf_delete(bufnr, { force = true })
+        end
+    })
+
+    vim.cmd("q!")
+    vim.cmd("buffer " .. bufnr)
+
+end
+
+M.list_rfcs = function(opts)
+    opts = opts or {
+        layout_strategy = "vertical",
+        layout_config = {
+            width = 0.7,
+            height = 0.5,
+        },
+    }
+
+    local file_ref = io.open(config.rfc_dir .. "/rfc-ref.txt", "r")
+    local content_ref = file_ref:read("*a")
+    file_ref:close()
+
+    pickers.new(opts, {
+        prompt_title = "RFCs",
+
+        finder = finders.new_table {
+            results = vim.split(content_ref, "\n"),
+        },
+
+        sorter = sorters.get_generic_fuzzy_sorter({}),
+
+        attach_mappings = function(_, map)
+            local function open_rfc()
+                local selection = action_state.get_selected_entry()[1]
+                local rfc = string.match(selection, "RFC(%d+)")
+                local file = config.rfc_dir .. "/rfc" .. rfc .. ".txt"
+
+                if not file_exists(file) then
+                    M.download_rfc(rfc)
+                end
+
+                open_rfc_buf(file, rfc)
+            end
+
+            map("i", "<CR>", open_rfc)
+            map("n", "<CR>", open_rfc)
+
+            return true
+
+        end,
+    }):find()
 
 end
 
